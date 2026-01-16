ffb39d96b95785b77abe98fcd1c3dac4304e8ead
asukakuwahara <kuwahara.mee@gmail.com>
2026-01-16 09:23:23 +0900

    remove unused arg

--- cognitive_oddballs/main.py
+++ cognitive_oddballs/main.py
@@ -1,4 +1,5 @@
 from environments.change_point_oddball_environment import generate_oddball_environment
+from models.change_point_nassar_2016 import ChangePointNassarModel
 from visualizer import visualize_environment, visualize_summary
 
 
@@ -15,6 +16,9 @@ def main():
     # Show summary
     visualize_summary(df)
 
+    nassar_model = ChangePointNassarModel(X=df['x'], sigma_sequence=df['sigma'] )
+    normative_model_results = nassar_model.run()
+    print(normative_model_results)
 
 if __name__ == "__main__":
     main()
