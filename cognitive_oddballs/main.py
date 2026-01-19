73f7433c607cfe6e9cdfe55364e09c0313910939
Jannes Bruns <62057914+JannesB24@users.noreply.github.com>
2026-01-19 17:01:41 +0100

    Refactor the project (#12)
    * Add initial environment and model structure; refactor imports and evaluation logic
    
    * add to readme
    
    ---------
    
    Co-authored-by: Jannes <jannbruns@uni-osnabrueck.de>

--- cognitive_oddballs/main.py
+++ cognitive_oddballs/main.py
@@ -1,5 +1,15 @@
+from cognitive_oddballs.environments.change_point_oddball import generate_change_point_environment
+from cognitive_oddballs.environments.random_walk_oddball import generate_random_walk_environment
+
+
 def main():
-    pass
+    _ = generate_change_point_environment(
+        n_trials=100, change_point_hazard_rate=0.1, oddball_hazard_rate=0.2, sigma=25, seed=42
+    )
+
+    _ = generate_random_walk_environment(
+        n_trials=100, drift_sigma=5, oddball_hazard_rate=0.02, sigma=25, seed=42
+    )
 
 
 if __name__ == "__main__":
