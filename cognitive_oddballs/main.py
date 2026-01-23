c6cd6a7cdb799e7b35e9290d9c816b11790c7f30
Asuka Kuwahara <42548220+asukakuwahara@users.noreply.github.com>
2026-01-23 22:57:27 +0900

    Remove the testing chunk from the main.py and apply it in change_point (#15)
    * Remove the testing chunk from the main.py and apply it in change_point_nassar 2016
    
    * Updated parameters: n_trials=400, sigma=25, seed=555; added mu and env_df to plot oddballs and true position
    
    ---------
    
    Co-authored-by: danikamq <danikamq@gmail.com>

--- cognitive_oddballs/main.py
+++ cognitive_oddballs/main.py
@@ -1,7 +1,6 @@
 from cognitive_oddballs.environments.change_point_oddball import generate_change_point_environment
 from cognitive_oddballs.environments.random_walk_oddball import generate_random_walk_environment
 
-
 def main():
     _ = generate_change_point_environment(
         n_trials=100, change_point_hazard_rate=0.1, oddball_hazard_rate=0.2, sigma=25, seed=42
@@ -10,11 +9,6 @@ def main():
     _ = generate_random_walk_environment(
         n_trials=100, drift_sigma=5, oddball_hazard_rate=0.02, sigma=25, seed=42
     )
-
-    nassar_model = ChangePointNassarModel(X=df["x"], sigma_sequence=df["sigma"])
-    normative_model_results = nassar_model.run()
-    print(normative_model_results)
-
 
 if __name__ == "__main__":
     main()
