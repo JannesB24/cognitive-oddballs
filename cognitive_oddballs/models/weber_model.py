8bae64715e9c19ac241f86298acbe9a53df38f87
luheuer <luheuer@uos.de>
2026-02-06 12:02:34 +0100

    updated model to be more adaptable for testing + added "drops_out()"

--- cognitive_oddballs/models/weber_model.py
+++ cognitive_oddballs/models/weber_model.py
@@ -6,38 +6,47 @@ from cognitive_oddballs.models.model import Model
 
 
 class WeberModel(Network, Model):
-    def __init__(self, node3=False, n0_tv=5,n1_tv=0,n2_tv=0, update_type="eHGF"):
+    def __init__(self, n_nodes=4, x_0_tv=5, x_2_mu=15, x_3_mu = 40, x_4_p=1e1, update_type="eHGF"): #paramteres to maybe add: node precisions, tonic volatilities, initial means for node 3 and 4 (if 4 is kept)
         """A subclass of the pyhfg Network class.
-        Each Instance is a set network of either 3 or 4 Nodes (4th node can be left out for
+        Each Instance is a set network of 3 to 5 Nodes (4th and 5th node can be left out for
         experimental purposes):
-            Node 0: Continous input Node
-            Node 1: Value Parent of Node 0
-            Node 2: Volatility Parent of Node 1
-            Node 3: Volatility Parent of Node 0
+
+            Node 0: Continous input Node | represents the expected bag drop location
+            Node 1: Value Parent of Node 0 | represents the expected location of the helicopter
+            Node 2: Volatility Parent of Node 1 | represents the volatility of helicopter movement (spikes in trajectory noting changepoints)
+            Node 3: Volatility Parent of Node 0  | represents the volatility of the bag drop locations (spikes in trajectory noting oddballs?)
+            Node 4: Volatility Parent of Node 3
 
             Input:
-            - input: (optional) a Dataframe containing observations (in a column named 'x'),
-            to which the model is fit immediately.
-            - node3: A Boolean indicating whether the model contains node 3 (default False)
-            - node_4_type: a string indicating whether node 4 is supposed to be a value parent or
-              a volatility parent (default volatility parent)
-            - n4_p: precision of node 4, if used (default 3, as that was the sweetspot for model
-              performance in both environments)
+            - n_nodes: An int denominating the number of nodes ( 3 to 5 nodes (default 4))
+            - x_0_tv: tonic volatility of node 0 (default 5)
+            - x_2_mu: initial mean of node 2 (default 15)
+            - x_3_mu: initial mean of node 3 (default 40)
+            - x_4_p: precision of node 4 (default 1e1; only needed if n_nodes == 5)
+            - ....
+            - update_type: the update type forwarded to the Network class (default currently "eHGF")
+
 
         """
+        #if n_nodes is not in specified range, a value error is raised
+        if n_nodes <3 or n_nodes >5:
+            raise ValueError("n_nodes must be between 3 and 5 (inclusive)")
+
+        # passing the update type to the Init function of the Network class
         super().__init__(update_type=update_type)
-        self.add_nodes(mean=250, tonic_volatility=5, autoconnection_strength=0)
-        self.add_nodes(mean=250, value_children=0)
-        self.add_nodes(mean=15,volatility_children=1)
-        if node3:
-            self.add_nodes(mean=40,volatility_children=0)
+        # Node 0: Observation node/ Continuous input node
+        self.add_nodes(mean=250, tonic_volatility=x_0_tv, autoconnection_strength=0) # initial mean set at 250, as that is always the middle of the possible environmental values
+        # Node 1: Value parent of Node 0
+        self.add_nodes(mean=250, value_children=0)  # initial mean set at 250, as that is always the middle of the possible environmental values
+        # Node 2: Volatility parent of node 1
+        self.add_nodes(mean=x_2_mu,volatility_children=1)
+        # if given number of nodes is at least 4, more nodes are added
+        if n_nodes >=4:
+            self.add_nodes(mean=x_3_mu,volatility_children=0)
+            # if number of nodes is 5 one more node is added
+            if n_nodes ==5:
+                self.add_nodes(volatility_children=3, precision=x_4_p)
 
-
-    # precision 3 seems to be the sweet spot so far, such that the first 500 trials can be predicted
-    # and are shown in graph (more trials still not working)
-    # -> dicotomy between two environments with random walk environment giving up earlier with
-    # higher preciscion
-    # -> works fine if node 4 is removed
 
     def run(self, observations: np.ndarray) -> pd.DataFrame:
         self.input_data(observations)
@@ -48,17 +57,23 @@ class WeberModel(Network, Model):
 
         return output.rename(columns=rename_dict)
 
-    # both fitting functions currently the same, but should maybe stay separate for usability
-    def fit_to_change_point_oddball_environment(self, df):
-        """Fitting the gHGF to a given dataset, produced in a change-point oddball environment"""
+    def to_pandas(self):
+        """Returns the trajectories of the nodes. Extended with the prediction error of node 0"""
+        output = super().to_pandas()
+        output["x_0_prediction_error"] = output["x_0_mean"] - output["x_0_expected_mean"]
+        return output
+    
 
-        input = df["x"].to_numpy()
-        self.input_data(input)
+    # functions used in testing
+    def drops_out(self) -> bool:
+        """Checks whether the Model drops out at any point and returns the corresponding bool"""
+        trajectories = self.to_pandas()
 
-    def fit_to_random_walk_oddball_environment(self, df):
-        """Fits the gHGF onto data generated in a random walk oddball environment"""
-        input = df["x"].to_numpy()
-        self.input_data(input)
+        return (np.count_nonzero(np.isnan(trajectories)) > 0)
+    
+    def value_prediction_errors(self):
+        """Returns the prediction error of Node 0 for each observation"""
+        return self.to_pandas()["x_0_prediction_error"]
 
     def largest_jump(self):
         """Returns the largest jump in the observations and its index
@@ -94,16 +109,24 @@ class WeberModel(Network, Model):
                 at = i
             i = i + 1
         return max_total_surprise, at
+    
+#### outdated functions (can potentially be removed)
 
-    def value_prediction_errors(self):
-        """Returns the prediction error of Node 0 for each observation"""
-        return self.to_pandas()["x_0_prediction_error"]
+    # both fitting functions redundant, as input_data() from parent class can be used
 
-    def to_pandas(self):
-        output = super().to_pandas()
-        output["x_0_prediction_error"] = output["x_0_mean"] - output["x_0_expected_mean"]
-        return output
+    # # both fitting functions currently the same, but should maybe stay separate for usability
+    # def fit_to_change_point_oddball_environment(self, df):
+    #     """Fitting the gHGF to a given dataset, produced in a change-point oddball environment"""
 
+    #     input = df["x"].to_numpy()
+    #     self.input_data(input)
+
+    # def fit_to_random_walk_oddball_environment(self, df):
+    #     """Fits the gHGF onto data generated in a random walk oddball environment"""
+    #     input = df["x"].to_numpy()
+    #     self.input_data(input)
+
+    # replaced by run()
     def get_outputs(self):
         trajectories = self.to_pandas()
         outputs = {"prediction_errors": trajectories["x_0_prediction_error"], "updates": []}
